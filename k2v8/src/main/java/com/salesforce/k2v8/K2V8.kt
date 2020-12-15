6ff6b154b139e21b7f5102f638a6f047e341d9d1
Nadeem Mohammed <nmohammed@salesforce.com>
2020-12-14 19:21:02 -0800

    Update Utils and K2V8

--- k2v8/src/main/java/com/salesforce/k2v8/K2V8.kt
+++ k2v8/src/main/java/com/salesforce/k2v8/K2V8.kt
@@ -8,27 +8,30 @@
 package com.salesforce.k2v8
 
 import com.eclipsesource.v8.V8Object
-import com.eclipsesource.v8.utils.MemoryManager
 import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.SerialFormat
 import kotlinx.serialization.SerializationStrategy
-import kotlinx.serialization.modules.SerialModule
+import kotlinx.serialization.modules.EmptySerializersModule
+import kotlinx.serialization.modules.SerializersModule
 
 /**
  * The main entry point for V8Object serialization.
  */
-class K2V8(val configuration: Configuration, override val context: SerialModule = DefaultModule) :
-    SerialFormat {
+class K2V8(val configuration: Configuration, override val serializersModule: SerializersModule = EmptySerializersModule) : SerialFormat {
 
     /**
      * Serializes a [T] value to a [V8Object] using the [serializer] provided.
      */
+
+    @InternalSerializationApi
     fun <T> toV8(serializer: SerializationStrategy<T>, value: T) =
-        convertToV8Object(value, serializer)
+            convertToV8Object(value, serializer)
 
     /**
      * Deserializes a [V8Object] value to a [T] using the [deserializer] provided.
      */
+    @InternalSerializationApi
     fun <T> fromV8(deserializer: DeserializationStrategy<T>, value: V8Object): T {
         return convertFromV8Object(value, deserializer)
     }
